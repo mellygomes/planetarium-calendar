4013150b1780f5bbd4491c31be47ac9cb6f8c5c5
Emanuelly <emanuellygomes.manu2004@gmail.com>
2025-05-26 13:31:46 -0300

    feat(api): adiciona api rest através de blueprints

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask # Flask para criar o site
+from api.routes import api_bp
+
 app = Flask(__name__)
+app.register_blueprint(api_bp)
 
 #rotas
 from routes.routes import *
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
