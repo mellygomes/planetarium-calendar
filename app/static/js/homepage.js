47eaaaa8123dfc4caae47e3a3bde2bbc5029a3f7
Deborah-Araujo <deby.limara@gmail.com>
2025-06-20 01:55:24 -0300

    holding local changes

--- app/static/js/homepage.js
+++ app/static/js/homepage.js
@@ -1,1 +1,23 @@
-document.getElementsByTagName('p')
+// document.getElementsByTagName('p')
+
+document.addEventListener('DOMContentLoaded', function () {
+  new Swiper('.carousel-events', {
+    slidesPerView: 5,
+    spaceBetween: 28,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 5,
+      }
+    }
+  });
+});
