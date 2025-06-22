eefab7b89f5c006bcc1868197b9fd3570eea1e06
Emanuelly <emanuellygomes.manu2004@gmail.com>
2025-06-22 14:28:04 -0300

    feat: merge routes e controllers

--- app/controllers/evento_controller.py
+++ app/controllers/evento_controller.py
@@ -50,48 +50,76 @@ def editar_evento():
     pass
 
     
+import json
+import ephem
+from datetime import timedelta
 
-def render_calendario_html(calendario, ano):
-    html = ""
+def calcular_dia_mundial_astronomia(ano):
+    equinocio = datetime(ano, 9, 22)
+    proximo_quarto_crescente = ephem.next_first_quarter_moon(ephem.Date(equinocio)).datetime()
 
-    for mes, semanas in calendario.items():
-        nome_mes = calendar.month_name[mes]
+    dias_para_sabado = (5 - proximo_quarto_crescente.weekday()) % 7
+    sabado = proximo_quarto_crescente + timedelta(days=dias_para_sabado)
 
-        if (mes - 1) % 3 == 0:
-            html += "<div class='row'>"
+    if abs((sabado - proximo_quarto_crescente).days) > 3:
+        sabado -= timedelta(days=7)
 
-        html += f"<div class='mes-container col-4'> <a href='/calendario/{ano}-{mes}'><h4 class='bosta'>{nome_mes}</h4><table class='table table-bordered text-center'>"
-        html += "<thead><tr>" + "".join(f"<th>{dia}</th>" for dia in ["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"]) + "</tr></thead><tbody>"
+    return sabado.date()
 
-        for semana in semanas:
-            html += "<tr>" + "".join(f"<td><div class='dia'><p>{dia if dia != 0 else '&nbsp'}</p></div></td>" for dia in semana) + "</tr>"
 
-        html += f"</tbody></table></a></div>"
+def carregar_eventos_astronomicos(ano):
+    with open("app/datas_astronomicas.json", "r", encoding="utf-8") as f:
+        datas = json.load(f)
 
-        # fecha a linha a cada 3 meses
-        if mes % 3 == 0:
-            html += "</div>"
+    eventos = {}
 
-    # caso o último <div class="row"> não tenha sido fechado (ex: se o loop acabar no mês 11)
-    if mes % 3 != 0:
-        html += "</div>"
+    for evento in datas:
+        if evento.get("movel"):
+            data = calcular_dia_mundial_astronomia(ano)
+        elif evento.get("data"):
+            dia, mes = map(int, evento["data"].split("-"))
+            data = datetime(ano, mes, dia).date()
+        else:
+            continue  # ignora eventos sem data válida
 
-    return html
+        eventos[str(data)] = evento["nome"]
+
+    return eventos
+
+
 
 def render_mes_html(ano, mes):
-    html = ""
+    eventos = carregar_eventos_astronomicos(ano)
 
     nome_mes = calendar.month_name[mes]
     semanas = calendar.monthcalendar(ano, mes)
 
-    html += f"<div class='mes-unico-container'><h4>{nome_mes} {ano}</h4><table class='table table-bordered text-center'>"
+    html = f"<div class='mes-unico-container'><h4>{nome_mes} {ano}</h4><table class='table table-bordered text-center'>"
     html += "<thead><tr>" + "".join(f"<th>{dia}</th>" for dia in ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"]) + "</tr></thead><tbody>"
 
     for semana in semanas:
-        html += "<tr>" + "".join(f"<td><div class='dia'><p>{dia if dia != 0 else '&nbsp'}</p></div></td>" for dia in semana) + "</tr>"
+        html += "<tr>"
+        for dia in semana:
+            if dia == 0:
+                html += "<td><div class='dia'>&nbsp;</div></td>"
+            else:
+                data_str = f"{ano}-{mes:02d}-{dia:02d}"
+                nome_evento = eventos.get(data_str)
+
+                if nome_evento:
+                    html += f"""
+                    <td>
+                        <div class='dia evento-astronomico' data-dia='{dia}'>
+                            <p>{dia}</p>
+                            <div class='nome-evento'>{nome_evento}</div>
+                        </div>
+                    </td>
+                    """
+                else:
+                    html += f"<td><div class='dia' data-dia='{dia}'><p>{dia}</p></div></td>"
+        html += "</tr>"
 
     html += "</tbody></table></div>"
-
     return html
 
 # ---------------------------------------------------------- admin
